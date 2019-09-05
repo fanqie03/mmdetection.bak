d978e4de7dd273705a87464442908568320ceeb5
mengfu188 <1242733702@qq.com>
2019-09-05 12:06:16 +0800

    - add fps display
    - delete type int in camera-id

--- demo/webcam_demo.py
+++ demo/webcam_demo.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 import cv2
 import torch
@@ -12,7 +13,7 @@ def parse_args():
     parser.add_argument('checkpoint', help='checkpoint file')
     parser.add_argument('--device', type=int, default=0, help='CUDA device id')
     parser.add_argument(
-        '--camera-id', type=int, default=0, help='camera device id')
+        '--camera-id', default=0, help='camera device id')
     parser.add_argument(
         '--score-thr', type=float, default=0.5, help='bbox score threshold')
     args = parser.parse_args()
@@ -27,6 +28,10 @@ def main():
 
     camera = cv2.VideoCapture(args.camera_id)
 
+    frame_rate = 0
+    start_time = time.time()
+    frame_count = 0
+
     print('Press "Esc", "q" or "Q" to exit.')
     while True:
         ret_val, img = camera.read()
@@ -35,6 +40,18 @@ def main():
         ch = cv2.waitKey(1)
         if ch == 27 or ch == ord('q') or ch == ord('Q'):
             break
+
+        end_time = time.time()
+        if (end_time - start_time) >= 1:
+            frame_rate = int(frame_count / (end_time - start_time))
+            start_time = time.time()
+            frame_count = 0
+
+        cv2.putText(img, str(frame_rate) + " fps", (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),
+                    thickness=2, lineType=2)
+
+        frame_count += 1
 
         show_result(
             img, result, model.CLASSES, score_thr=args.score_thr, wait_time=1)
