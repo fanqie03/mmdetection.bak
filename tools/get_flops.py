8d5c1c888e83507361204bcdea8e0e89f4764441
mengfu188 <1242733702@qq.com>
2019-09-05 12:07:52 +0800

    - disable pretrained model in get_flops.py

--- tools/get_flops.py
+++ tools/get_flops.py
@@ -31,6 +31,7 @@ def main():
         raise ValueError('invalid input shape')
 
     cfg = Config.fromfile(args.config)
+    cfg.model.pretrained = None
     model = build_detector(
         cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg).cuda()
     model.eval()
