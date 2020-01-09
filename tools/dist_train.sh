c39a1c534c2236e41e9d139729ca083e1b871fe8
mengfu188 <1242733702@qq.com>
2020-01-09 11:45:53 +0800

    Update dist_train.sh

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -7,4 +7,4 @@ GPUS=$2
 PORT=${PORT:-29500}
 
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+    tools/train.py $CONFIG --launcher pytorch --validate
